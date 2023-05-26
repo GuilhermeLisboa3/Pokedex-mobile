0ed8b199fbc5a12d948332676201fd4ae8ac2fc6
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-26 13:42:25 -0300

    test: ensure AxiosAdapter call axios with correct value

---
+++ tests/infra/http/axios-adapter.spec.ts
@@ -0,0 +1,23 @@
+import { AxiosAdapter } from '@/infra/http'
+import { httpClientParams } from '@/tests/mocks'
+
+import axios from 'axios'
+
+jest.mock('axios')
+
+describe('AxiosAdapter', () => {
+  const fakeAxios = axios as jest.Mocked<typeof axios>
+  const { method, body, url } = httpClientParams
+  let sut: AxiosAdapter
+
+  beforeEach(() => {
+    sut = new AxiosAdapter()
+  })
+
+  it('should call axios with correct value', async () => {
+    await sut.request({ method, body, url })
+
+    expect(fakeAxios.request).toHaveBeenCalledWith({ method, data: body, url })
+    expect(fakeAxios.request).toHaveBeenCalledTimes(1)
+  })
+})
