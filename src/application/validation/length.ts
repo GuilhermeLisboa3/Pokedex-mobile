bc20c8b504ee4a54a8ce9bddf82cff4ae5631e99
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-26 14:03:05 -0300

    feat: ensure LengthValidation return error if value is empty

---
+++ src/application/validation/length.ts
@@ -0,0 +1,10 @@
+import { InvalidFieldError } from './errors'
+import { type FieldValidator } from './field-validator'
+
+export class LengthValidation implements FieldValidator {
+  constructor (readonly field: string, private readonly length: number) {}
+
+  validate (input: object): Error | undefined {
+    if ((input as any)[this.field] && (input as any)[this.field]?.length !== this.length) return new InvalidFieldError()
+  }
+}
