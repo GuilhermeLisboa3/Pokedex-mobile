9ab3b1eacf4327832725963c059b83ddf54e3f8b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-26 13:12:29 -0300

    chore: add babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo', '@babel/preset-typescript']
+    presets: ['babel-preset-expo', '@babel/preset-typescript'],
+    plugins: [
+      ['module-resolver', {
+        root: ['./src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        alias: {
+          '@/tests': './tests',
+          '@': './src'
+        }
+      }]
+    ]
   }
 }
