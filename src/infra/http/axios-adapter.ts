b4ffc7fc7aee939849c518711294b102b325b4a2
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-26 13:45:48 -0300

    feat: ensure AxiosAdapter return correct error

--- src/infra/http/axios-adapter.ts
+++ src/infra/http/axios-adapter.ts
@@ -1,10 +1,13 @@
-import { type HttpRequest, type HttpResponse } from '@/domain/contracts/http'
+import { type HttpResponse, type HttpRequest, type HttpClient } from '@/domain/contracts/http'
 
-import axios from 'axios'
+import axios, { type AxiosResponse } from 'axios'
 
-export class AxiosAdapter {
+export class AxiosAdapter implements HttpClient {
   async request ({ method, url, body, headers }: HttpRequest): Promise<HttpResponse> {
-    const { status, data } = await axios.request({ url, method, data: body, headers })
-    return { statusCode: status, data }
+    let axiosResponse: AxiosResponse
+    try {
+      axiosResponse = await axios.request({ url, method, data: body, headers })
+    } catch (error: any) { axiosResponse = error.response }
+    return { statusCode: axiosResponse.status, data: axiosResponse.data }
   }
 }
