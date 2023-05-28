94134cd5ae89e8715447f536f8d4a959c1875084
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-28 14:13:08 -0300

    feat: ensure SignUp success input if Validator return undefined

--- src/application/pages/signup/index.tsx
+++ src/application/pages/signup/index.tsx
@@ -12,14 +12,18 @@ type Props = {
 
 export const SignUp: React.FC<Props> = ({ validator }) => {
   const [name, setName] = useState('')
+  const [nameError, setNameError] = useState<string | undefined>('')
   const [email, setEmail] = useState('')
+  const [emailError, setEmailError] = useState<string | undefined>('')
   const [password, setPassword] = useState('')
+  const [passwordError, setPasswordError] = useState<string | undefined>('')
   const [passwordConfirmation, setPasswordConfirmation] = useState('')
+  const [passwordConfirmationError, setPasswordConfirmationError] = useState<string | undefined>('')
 
-  useEffect(() => { validator.validate('name', { name }) }, [name])
-  useEffect(() => { validator.validate('email', { email }) }, [email])
-  useEffect(() => { validator.validate('password', { password }) }, [password])
-  useEffect(() => { validator.validate('passwordConfirmation', { password, passwordConfirmation }) }, [password, passwordConfirmation])
+  useEffect(() => { setNameError(validator.validate('name', { name })) }, [name])
+  useEffect(() => { setEmailError(validator.validate('email', { email })) }, [email])
+  useEffect(() => { setPasswordError(validator.validate('password', { password })) }, [password])
+  useEffect(() => { setPasswordConfirmationError(validator.validate('passwordConfirmation', { password, passwordConfirmation })) }, [password, passwordConfirmation])
 
   return (
   <SafeAreaView>
@@ -32,10 +36,10 @@ export const SignUp: React.FC<Props> = ({ validator }) => {
           <Title>Seja bem vindo(a) {'\n'} a Pokedex Pokemon</Title>
           <Image source={logo} resizeMode='stretch'/>
             <ContainerInputs>
-              <Input setChange={setName} testID='name' isError={'error'} iconLeft iconNames={'user'} iconSize={20} placeholder='Digite seu nome'/>
-              <Input setChange={setEmail} testID='email' isError={'error'} iconLeft iconNames={'mail'} iconSize={20} placeholder='Digite seu email'/>
-              <Input setChange={setPassword} testID='password' isError={'error'} iconLeft iconNames={'lock'} iconSize={20} placeholder='Digite sua senha' iconViewPassword secureTextEntry/>
-              <Input setChange={setPasswordConfirmation} testID='passwordConfirmation' isError={'error'} iconLeft iconNames={'lock'} iconSize={20} placeholder='Confirme sua senha' iconViewPassword secureTextEntry/>
+              <Input setChange={setName} testID='name' isError={nameError} iconLeft iconNames={'user'} iconSize={20} placeholder='Digite seu nome'/>
+              <Input setChange={setEmail} testID='email' isError={emailError} iconLeft iconNames={'mail'} iconSize={20} placeholder='Digite seu email'/>
+              <Input setChange={setPassword} testID='password' isError={passwordError} iconLeft iconNames={'lock'} iconSize={20} placeholder='Digite sua senha' iconViewPassword secureTextEntry/>
+              <Input setChange={setPasswordConfirmation} testID='passwordConfirmation' isError={passwordConfirmationError} iconLeft iconNames={'lock'} iconSize={20} placeholder='Confirme sua senha' iconViewPassword secureTextEntry/>
             </ContainerInputs>
             <Button width={150} height={40} text='Registrar' disabled/>
           <TextLink>Você tem conta? <NavigationLink>Entrar</NavigationLink></TextLink>
