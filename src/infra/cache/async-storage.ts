0bd6487a1b80d87cb86da9b8f7285538880c83b1
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-01 15:22:22 -0300

    feat: ensure AsyncStorageAdapter call localStorage.removeItem if value is null

--- src/infra/cache/async-storage.ts
+++ src/infra/cache/async-storage.ts
@@ -3,6 +3,10 @@ import storage from '@react-native-async-storage/async-storage'
 
 export class AsyncStorageAdapter {
   async set ({ key, value }: SetStorage.Input): Promise<void> {
-    await storage.setItem(key, JSON.stringify(value))
+    if (value) {
+      await storage.setItem(key, JSON.stringify(value))
+    } else {
+      await storage.removeItem(key)
+    }
   }
 }
