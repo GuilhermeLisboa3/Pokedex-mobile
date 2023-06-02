90882a636ab65ca8f50cb95923a887d6758ff6bf
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-02 17:04:51 -0300

    test: ensure ListPokemonsUseCase return listPokemons on success

--- tests/domain/use-cases/api-pokemon/list-pokemons.spec.ts
+++ tests/domain/use-cases/api-pokemon/list-pokemons.spec.ts
@@ -10,7 +10,7 @@ describe('ListPokemonsUseCase', () => {
   const perPage: number = 25
   let sut: ListPokemons
   const { url } = httpClientParams
-  const { name } = ApiPokemonParams
+  const { name, id, height } = ApiPokemonParams
   const httpClient = mock<HttpClient>()
 
   beforeAll(() => {
@@ -39,5 +39,14 @@ describe('ListPokemonsUseCase', () => {
     await sut({ page, perPage })
 
     expect(httpClient.request).toHaveBeenNthCalledWith(2, { url: `${url}`, method: 'get' })
+  })
+
+  it('should return listPokemons on success', async () => {
+    httpClient.request
+      .mockResolvedValueOnce({ statusCode: 200, data: { results: [{ name }], count: 1 } })
+      .mockResolvedValueOnce({ statusCode: 200, data: { name, id, height } })
+    const result = await sut({ page, perPage })
+
+    expect(result).toEqual({ count: 1, pokemons: [{ name, id, height }] })
   })
 })
