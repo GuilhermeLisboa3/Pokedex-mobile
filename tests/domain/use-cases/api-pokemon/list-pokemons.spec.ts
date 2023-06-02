b345c31d566816c2de4d4abc615efe904181c216
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-02 16:52:20 -0300

    test: ensure ListPokemonsUseCase throw UnexpectedError if first HttpClient return error

--- tests/domain/use-cases/api-pokemon/list-pokemons.spec.ts
+++ tests/domain/use-cases/api-pokemon/list-pokemons.spec.ts
@@ -1,6 +1,7 @@
 import { type ListPokemons, ListPokemonsUseCase } from '@/domain/use-cases/api-pokemon'
 import { type HttpClient } from '@/domain/contracts/http'
 import { httpClientParams } from '@/tests/mocks'
+import { UnexpectedError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
@@ -11,6 +12,10 @@ describe('ListPokemonsUseCase', () => {
   const { url } = httpClientParams
   const httpClient = mock<HttpClient>()
 
+  beforeAll(() => {
+    httpClient.request.mockResolvedValue({ statusCode: 200 })
+  })
+
   beforeEach(() => {
     sut = ListPokemonsUseCase(url, httpClient)
   })
@@ -19,5 +24,13 @@ describe('ListPokemonsUseCase', () => {
     await sut({ page, perPage })
 
     expect(httpClient.request).toHaveBeenCalledWith({ url: `${url}/pokemon?limit=${perPage}&offset=${page}`, method: 'get' })
+  })
+
+  it('should throw UnexpectedError if first HttpClient return error', async () => {
+    httpClient.request.mockResolvedValueOnce({ statusCode: 500 })
+
+    const promise = sut({ page, perPage })
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
