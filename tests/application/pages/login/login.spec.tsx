54aca7929f556be9f7cbaa31afb16a2a35122fac
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-02 13:13:14 -0300

    test: ensure Login show alert error if Authentication fails

--- tests/application/pages/login/login.spec.tsx
+++ tests/application/pages/login/login.spec.tsx
@@ -3,8 +3,9 @@ import { AccountParams, populateField } from '@/tests/mocks'
 import { type Validator } from '@/application/validation'
 
 import React from 'react'
-import { fireEvent, render, screen } from '@testing-library/react-native'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react-native'
 import { type MockProxy, mock } from 'jest-mock-extended'
+import { InvalidCredentialsError } from '@/domain/errors'
 
 describe('SignUp', () => {
   const { email, password } = AccountParams
@@ -89,5 +90,15 @@ describe('SignUp', () => {
     populateFields()
 
     expect(authentication).not.toHaveBeenCalledTimes(1)
+  })
+
+  it('should show alert error if Authentication fails', async () => {
+    makeSut()
+    authentication.mockRejectedValueOnce(new InvalidCredentialsError())
+
+    simulateSubmit()
+    await waitFor(() => screen.getByTestId('toast'))
+
+    expect(screen.getByTestId('toast')).toBeTruthy()
   })
 })
