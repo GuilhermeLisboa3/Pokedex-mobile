09a4bbff47b2f196e910e0bea70d642f36c3dc72
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-05 15:46:52 -0300

    style: add EmptyCardPokemon component

---
+++ src/application/components/card-pokemon/components/empty/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components/native'
+import theme from '@/application/styles/theme'
+
+export const Container = styled.View.attrs((props) => ({
+  shadowColor: theme.COLORS.BOX_SHADOW,
+  shadowOffset: {
+    width: 0,
+    height: 13
+  },
+  shadowOpacity: 0.24,
+  shadowRadius: 14.86,
+  elevation: 18
+}))`
+  position: relative;
+  width: 280px;
+  margin-top: 100px;
+  height: 180px;
+  background-color: #ffffff;
+  border-radius: 10px;
+`
+
+export const Image = styled.View`
+  width: 77px;
+  height: 90px;
+`
