08744aa1567d9894013b97099259b87fc6f7b8d2
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-05 12:57:36 -0300

    feat: add footer component

---
+++ src/application/components/footer/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { Container, Link, Name, Navigate } from './styles'
+import { FontAwesome5 } from '@expo/vector-icons'
+import { Linking } from 'react-native'
+
+export const Footer: React.FC = () => {
+  const openLink = async (link: string): Promise<void> => await Linking.openURL(link)
+  return (
+    <Container>
+      <Name> <FontAwesome5 name='copyright' size={20} color='black'/> Guilherme Gonçalves Lisboa</Name>
+      <Navigate>
+        <Link onPress={() => { openLink('https://www.linkedin.com/in/guilherme-gon%C3%A7alves-lisboa-abb8b0227/') }}><FontAwesome5 name='linkedin-in' size={20} color='white'/></Link>
+        <Link onPress={() => { openLink('https://github.com/GuilhermeLisboa3') }}><FontAwesome5 name='github' size={20} color='white'/></Link>
+        <Link onPress={() => { openLink('https://www.instagram.com/guime.lisboa/') }}><FontAwesome5 name='instagram' size={20} color='white'/></Link>
+      </Navigate>
+    </Container>
+  )
+}
