00938bf4c0c44e7fabf008c7044c4f1aebff4f8e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-07 15:13:01 -0300

    test: ensure LinkToTop show

--- tests/application/components/link-to-top.spec.tsx
+++ tests/application/components/link-to-top.spec.tsx
@@ -9,4 +9,19 @@ describe('LinkToTop', () => {
 
     expect(screen.queryByTestId('arrowup')).toBeFalsy()
   })
+
+  it('should show LinkToTop', () => {
+    render(<LinkToTop
+      scrollMoveTop={jest.fn()}
+      eventScroll={{
+        contentInset: { bottom: 0, left: 0, right: 0, top: 0 },
+        contentOffset: { x: 0, y: 100 },
+        contentSize: { width: 0, height: 100 },
+        layoutMeasurement: { width: 0, height: 100 },
+        zoomScale: 0
+      }}
+      />)
+
+    expect(screen.queryByTestId('arrowup')).toBeTruthy()
+  })
 })
