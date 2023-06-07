658d2a0f758dfcf468d3c8a89c5a1e0113548a58
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-07 16:13:06 -0300

    feat: ensure GetDataPokemonUseCase return pokemons on success

--- src/domain/use-cases/api-pokemon/get-data-pokemon.ts
+++ src/domain/use-cases/api-pokemon/get-data-pokemon.ts
@@ -1,13 +1,17 @@
 import { type HttpResponse, type HttpClient } from '@/domain/contracts/http'
-import { type ApiPokemon } from '@/domain/models'
+import { type SpeciesPokemon, type ApiPokemon } from '@/domain/models'
 import { UnexpectedError } from '@/domain/errors'
 
 type Setup = (url: string, httpClient: HttpClient) => GetDataPokemon
 type Input = { idOrName: string }
-export type GetDataPokemon = (input: Input) => Promise<void>
+type Output = { pokemon: ApiPokemon, description: string }
+export type GetDataPokemon = (input: Input) => Promise<Output>
 
 export const GetDataPokemonUseCase: Setup = (url, httpClient) => async ({ idOrName }) => {
   const pokemon: HttpResponse<ApiPokemon> = await httpClient.request({ url: `${url}/pokemon/${idOrName}`, method: 'get' })
   if (pokemon.statusCode !== 200) throw new UnexpectedError()
-  await httpClient.request({ url: `${pokemon.data!.species.url}`, method: 'get' })
+  const pokemonDescription: HttpResponse<SpeciesPokemon> = await httpClient.request({ url: `${pokemon.data!.species.url}`, method: 'get' })
+  const description = pokemonDescription.data?.flavor_text_entries.filter(description => description.language.name === 'en')[0].flavor_text
+  const removesSymbols = description!.replace('', ' ')
+  return { pokemon: pokemon.data!, description: removesSymbols }
 }
