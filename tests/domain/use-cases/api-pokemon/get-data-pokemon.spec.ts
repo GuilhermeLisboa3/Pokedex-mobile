3508c3e8f82f23587c28e83d4b27be9ac548d95b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-07 15:58:18 -0300

    test: ensure GetDataPokemonUseCase throw UnexpectedError if HttpClient return 500

--- tests/domain/use-cases/api-pokemon/get-data-pokemon.spec.ts
+++ tests/domain/use-cases/api-pokemon/get-data-pokemon.spec.ts
@@ -1,14 +1,19 @@
 import { type GetDataPokemon, GetDataPokemonUseCase } from '@/domain/use-cases/api-pokemon'
 import { type HttpClient } from '@/domain/contracts/http'
 import { httpClientParams, ApiPokemonParams } from '@/tests/mocks'
+import { UnexpectedError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
 describe('GetDataPokemonUseCase', () => {
   let sut: GetDataPokemon
   const { url } = httpClientParams
-  const { name } = ApiPokemonParams
+  const { name, abilities, height, species } = ApiPokemonParams
   const httpClient = mock<HttpClient>()
+
+  beforeAll(() => {
+    httpClient.request.mockResolvedValue({ statusCode: 200, data: { name, abilities, height, species } })
+  })
 
   beforeEach(() => {
     sut = GetDataPokemonUseCase(url, httpClient)
@@ -18,5 +23,13 @@ describe('GetDataPokemonUseCase', () => {
     await sut({ idOrName: name })
 
     expect(httpClient.request).toHaveBeenCalledWith({ url: `${url}/pokemon/${name}`, method: 'get' })
+  })
+
+  it('should throw UnexpectedError if HttpClient return 500', async () => {
+    httpClient.request.mockResolvedValueOnce({ statusCode: 500 })
+
+    const promise = sut({ idOrName: name })
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
