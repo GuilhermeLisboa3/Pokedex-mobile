4e77d1814c5c89afc65d5ad202388fc83ef08cb8
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-07 11:00:49 -0300

    test: ensure Pagination not call setPage with correct value

--- tests/application/pages/home/components/pagination/pagination.spec.tsx
+++ tests/application/pages/home/components/pagination/pagination.spec.tsx
@@ -16,4 +16,16 @@ describe('Pagination', () => {
     fireEvent.press(screen.getAllByRole('link')[1])
     expect(setPageSpy).toHaveBeenCalledWith(1)
   })
+
+  it('should not call setPage with correct value', async () => {
+    render(<Pagination count={50} perPage={25} page={0} setPage={setPageSpy}/>)
+    fireEvent.press(screen.getAllByRole('link')[0])
+    expect(setPageSpy).not.toHaveBeenCalled()
+  })
+
+  it('should not call setPage with correct value', async () => {
+    render(<Pagination count={50} perPage={25} page={1} setPage={setPageSpy}/>)
+    fireEvent.press(screen.getAllByRole('link')[1])
+    expect(setPageSpy).not.toHaveBeenCalled()
+  })
 })
