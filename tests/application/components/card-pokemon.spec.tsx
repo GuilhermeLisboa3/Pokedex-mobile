e18d8b1fc974df2ac56e8ed0fe5a918ba634fe6d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-10 15:35:58 -0300

    feat: ensure CardPokemon call methods if click on card

--- tests/application/components/card-pokemon.spec.tsx
+++ tests/application/components/card-pokemon.spec.tsx
@@ -3,7 +3,7 @@ import { ApiPokemonParams } from '@/tests/mocks'
 import { CardAnimationContext } from '@/application/contexts'
 
 import React from 'react'
-import { render, screen } from '@testing-library/react-native'
+import { fireEvent, render, screen } from '@testing-library/react-native'
 
 describe('CardPokemon', () => {
   it('should render one type', () => {
@@ -25,5 +25,21 @@ describe('CardPokemon', () => {
 
     expect(screen.getByText('fire')).toBeTruthy()
     expect(screen.getByText('water')).toBeTruthy()
+  })
+
+  it('should call methods if click on card', () => {
+    const cardPokemonOpen = jest.fn()
+    const changeCardSize = jest.fn()
+    const dataPokemonOpen = jest.fn()
+    render(
+    <CardAnimationContext.Provider value={{ cardPokemonOpen, changeCardSize, dataPokemonOpen }}>
+      <CardPokemon pokemon={{ ...ApiPokemonParams, types: [{ type: { name: 'fire' } }, { type: { name: 'water' } }] }}/>
+    </CardAnimationContext.Provider>
+    )
+
+    fireEvent.press(screen.getByTestId('card-pokemon'))
+    expect(cardPokemonOpen).toHaveBeenCalledWith(false)
+    expect(dataPokemonOpen).toHaveBeenCalledWith(true)
+    expect(changeCardSize).toHaveBeenCalledWith(380, 800)
   })
 })
