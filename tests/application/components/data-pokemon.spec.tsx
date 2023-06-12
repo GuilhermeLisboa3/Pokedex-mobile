23ef775f30de6e7f8650b42ff3cc086a1beec8a0
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-12 11:28:18 -0300

    test: ensure DataPokemon render two type and ability

--- tests/application/components/data-pokemon.spec.tsx
+++ tests/application/components/data-pokemon.spec.tsx
@@ -16,4 +16,22 @@ describe('DataPokemon', () => {
     expect(screen.getByText(ApiPokemonParams.types[0].type.name)).toBeTruthy()
     expect(screen.getByText(ApiPokemonParams.abilities[0].ability.name)).toBeTruthy()
   })
+
+  it('should render two type and ability', () => {
+    render(
+      <CardAnimationContext.Provider value={{ cardPokemonOpen: jest.fn(), changeCardSize: jest.fn(), dataPokemonOpen: jest.fn() }}>
+        <DataPokemon pokemon={
+          {
+            ...ApiPokemonParams,
+            types: [{ type: { name: 'fire' } }, { type: { name: 'water' } }],
+            abilities: [{ ability: { name: 'static' } }, { ability: { name: 'bold' } }]
+          } } description='any'/>
+      </CardAnimationContext.Provider>
+    )
+
+    expect(screen.getByText('fire')).toBeTruthy()
+    expect(screen.getByText('water')).toBeTruthy()
+    expect(screen.getByText('static')).toBeTruthy()
+    expect(screen.getByText('bold')).toBeTruthy()
+  })
 })
