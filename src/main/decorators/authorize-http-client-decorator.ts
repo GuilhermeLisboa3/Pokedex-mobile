65f389daa2a208679fb08565704ddd0d598d54df
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-13 17:32:26 -0300

    feat: ensure AuthorizeHttpClientDecorator return the same result as HttpClient

--- src/main/decorators/authorize-http-client-decorator.ts
+++ src/main/decorators/authorize-http-client-decorator.ts
@@ -1,14 +1,14 @@
-import { type HttpRequest, type HttpClient } from '@/domain/contracts/http'
+import { type HttpRequest, type HttpClient, type HttpResponse } from '@/domain/contracts/http'
 import { type GetStorage } from '@/domain/contracts/cache'
 
-export class AuthorizeHttpClientDecorator {
+export class AuthorizeHttpClientDecorator implements HttpClient {
   constructor (private readonly getStorage: GetStorage, private readonly httpClient: HttpClient) {}
 
-  async request (data: HttpRequest): Promise<void> {
+  async request (data: HttpRequest): Promise<HttpResponse> {
     const account = await this.getStorage.get({ key: 'pokemon-token' })
     if (account?.token) {
       Object.assign(data, { headers: { ...data.headers, authorization: `Bearer ${account.token as string}` } })
     }
-    this.httpClient.request(data)
+    return await this.httpClient.request(data)
   }
 }
