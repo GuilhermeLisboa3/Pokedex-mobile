49f787ec3fe5a3c119ae51ce3e363257747e0b91
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-13 16:28:24 -0300

    test: ensure GetListFavoritePokemonUseCase throw AccessDeniedError HttpClient return 403

--- tests/domain/use-cases/pokemon/get-list-favorite-pokemon.spec.ts
+++ tests/domain/use-cases/pokemon/get-list-favorite-pokemon.spec.ts
@@ -1,6 +1,7 @@
 import { type GetListFavoritePokemon, GetListFavoritePokemonUseCase } from '@/domain/use-cases/pokemon'
 import { type HttpClient } from '@/domain/contracts/http'
 import { httpClientParams } from '@/tests/mocks'
+import { AccessDeniedError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
@@ -8,6 +9,10 @@ describe('GetListFavoritePokemonUseCase', () => {
   let sut: GetListFavoritePokemon
   const { url } = httpClientParams
   const httpClient = mock<HttpClient>()
+
+  beforeAll(() => {
+    httpClient.request.mockResolvedValue({ statusCode: 200 })
+  })
 
   beforeEach(() => {
     sut = GetListFavoritePokemonUseCase(url, httpClient)
@@ -18,5 +23,13 @@ describe('GetListFavoritePokemonUseCase', () => {
 
     expect(httpClient.request).toHaveBeenCalledWith({ url, method: 'get' })
     expect(httpClient.request).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw AccessDeniedError HttpClient return 403', async () => {
+    httpClient.request.mockResolvedValueOnce({ statusCode: 403 })
+
+    const promise = sut()
+
+    await expect(promise).rejects.toThrow(new AccessDeniedError())
   })
 })
