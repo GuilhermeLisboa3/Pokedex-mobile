91645ce39aa9e532c40240aeda36afe406585b45
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-13 16:31:51 -0300

    feat: ensure GetListFavoritePokemonUseCase return listFavoritePokemon if HttpClient return 200

--- src/domain/use-cases/pokemon/get-list-favorite-pokemon.ts
+++ src/domain/use-cases/pokemon/get-list-favorite-pokemon.ts
@@ -3,12 +3,13 @@ import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 import { type Pokemon } from '@/domain/models'
 
 type Setup = (url: string, httpClient: HttpClient<Pokemon[]>) => GetListFavoritePokemon
-export type GetListFavoritePokemon = () => Promise<void>
+type Output = Pokemon[]
+export type GetListFavoritePokemon = () => Promise<Output>
 
 export const GetListFavoritePokemonUseCase: Setup = (url, httpClient) => async () => {
-  const { statusCode } = await httpClient.request({ url, method: 'get' })
+  const { statusCode, data } = await httpClient.request({ url, method: 'get' })
   switch (statusCode) {
-    case 200: return undefined
+    case 200: return data!
     case 403: throw new AccessDeniedError()
     default: throw new UnexpectedError()
   }
