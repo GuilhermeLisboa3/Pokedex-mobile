41875b93dbb2f2b7bc002dad3e5cf3c5b7ed46c0
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-13 17:21:41 -0300

    feat: ensure AuthorizeHttpClientDecorator call GetStorage with correct value

---
+++ src/main/decorators/authorize-http-client-decorator.ts
@@ -0,0 +1,10 @@
+import { type HttpRequest, type HttpClient } from '@/domain/contracts/http'
+import { type GetStorage } from '@/domain/contracts/cache'
+
+export class AuthorizeHttpClientDecorator {
+  constructor (private readonly getStorage: GetStorage, private readonly httpClient: HttpClient) {}
+
+  async request (data: HttpRequest): Promise<void> {
+    this.getStorage.get({ key: 'pokemon-token' })
+  }
+}
