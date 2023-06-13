5a4093efe9407890c8b93c50172853c631b2bb3c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-13 17:22:52 -0300

    test: ensure AuthorizeHttpClientDecorator call GetStorage with correct value

---
+++ tests/main/decorators/authorize-http-client-decorator.spec.ts
@@ -0,0 +1,23 @@
+import { type GetStorage } from '@/domain/contracts/cache'
+import { type HttpClient } from '@/domain/contracts/http'
+import { AuthorizeHttpClientDecorator } from '@/main/decorators'
+import { httpClientParams } from '@/tests/mocks'
+
+import { mock } from 'jest-mock-extended'
+
+describe('AuthorizeHttpClientDecorator', () => {
+  const { body, method, url, headers } = httpClientParams
+  let sut: AuthorizeHttpClientDecorator
+  const getStorage = mock<GetStorage>()
+  const httpClient = mock<HttpClient>()
+
+  beforeEach(() => {
+    sut = new AuthorizeHttpClientDecorator(getStorage, httpClient)
+  })
+
+  it('should call GetStorage with correct value', async () => {
+    await sut.request({ body, method, url, headers })
+
+    expect(getStorage.get).toHaveBeenCalledWith({ key: 'pokemon-token' })
+  })
+})
