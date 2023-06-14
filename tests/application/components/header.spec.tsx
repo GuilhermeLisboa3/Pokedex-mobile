73af5a4db9e9439f2ba7ec73bb73503844d9dc17
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-14 11:22:09 -0300

    test: ensure Header load the Auth header if it has token

--- tests/application/components/header.spec.tsx
+++ tests/application/components/header.spec.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react-native'
 
 describe('Header', () => {
-  const getSpy = jest.fn().mockResolvedValue(null)
+  let getSpy = jest.fn()
   const makeSut = (): void => {
     render(
       <AccountContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: getSpy }}>
@@ -14,9 +14,19 @@ describe('Header', () => {
     )
   }
 
+  beforeAll(() => {
+    getSpy = jest.fn().mockResolvedValue(null)
+  })
+
   it('should load the NoAuth header if it has no token', () => {
     makeSut()
     expect(screen.getByText('Entrar')).toBeTruthy()
     expect(screen.getByText('Registrar')).toBeTruthy()
+  })
+
+  it('should load the Auth header if it has token', async () => {
+    getSpy.mockResolvedValueOnce({ token: 'any_token', name: 'any_name', email: 'any_email' })
+    makeSut()
+    expect(await screen.findAllByTestId('auth-link')).toBeTruthy()
   })
 })
