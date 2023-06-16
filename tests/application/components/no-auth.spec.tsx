adb891fd92dd0db5ec3e9618bb20d135cc2973b9
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-16 14:41:35 -0300

    test: ensure NoAuth redirect to the Login screen if click on Entrar

---
+++ tests/application/components/no-auth.spec.tsx
@@ -0,0 +1,30 @@
+import { NoAuth } from '@/application/components/header/components'
+
+import React from 'react'
+import { fireEvent, render, screen } from '@testing-library/react-native'
+import Navigation from '@react-navigation/native'
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    useNavigation: () => ({
+      navigate: jest.fn()
+    })
+  }
+})
+
+describe('NoAuth', () => {
+  const useNavigation = jest.spyOn(Navigation, 'useNavigation')
+  const navigation = { navigate: jest.fn() }
+
+  const makeSut = (): void => { render(<NoAuth/>) }
+
+  beforeAll(() => {
+    useNavigation.mockReturnValue(navigation)
+  })
+
+  it('should redirect to the Login screen if click on Entrar', async () => {
+    makeSut()
+    fireEvent.press(screen.getByText('Entrar'))
+    expect(navigation.navigate).toHaveBeenCalledWith('Login')
+  })
+})
