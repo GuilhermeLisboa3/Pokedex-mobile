834e29b5d773b8b299ee45c87d795a79cb74a123
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-16 15:56:46 -0300

    feat: ensure AddPokemonUseCase throw UnexpectedError if HttpClient return 500

--- src/domain/use-cases/pokemon/add-pokemon.ts
+++ src/domain/use-cases/pokemon/add-pokemon.ts
@@ -1,5 +1,5 @@
 import { type HttpClient } from '@/domain/contracts/http'
-import { AccessDeniedError } from '@/domain/errors'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 import { type Pokemon } from '@/domain/models'
 
 type Setup = (url: string, httpClient: HttpClient<boolean>) => AddPokemon
@@ -9,6 +9,8 @@ export type AddPokemon = (input: Input) => Promise<void>
 export const AddPokemonUseCase: Setup = (url, httpClient) => async (pokemon) => {
   const { statusCode } = await httpClient.request({ url, method: 'post', body: pokemon })
   switch (statusCode) {
+    case 200: return undefined
     case 403: throw new AccessDeniedError()
+    default: throw new UnexpectedError()
   }
 }
