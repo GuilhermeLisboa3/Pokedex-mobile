e3cd45b094fb84ca67765bd06e9c41d87048ab0f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-17 10:06:52 -0300

    test: ensure DeletePokemonUseCase throw AccessDeniedError HttpClient return 403

--- tests/domain/use-cases/pokemon/delete-pokemon.spec.ts
+++ tests/domain/use-cases/pokemon/delete-pokemon.spec.ts
@@ -3,6 +3,7 @@ import { type HttpClient } from '@/domain/contracts/http'
 import { httpClientParams, PokemonParams } from '@/tests/mocks'
 
 import { mock } from 'jest-mock-extended'
+import { AccessDeniedError } from '@/domain/errors'
 
 describe('DeletePokemonUseCase', () => {
   let sut: DeletePokemon
@@ -23,5 +24,13 @@ describe('DeletePokemonUseCase', () => {
 
     expect(httpClient.request).toHaveBeenCalledWith({ url: `${url}/${idPokemon}`, method: 'delete' })
     expect(httpClient.request).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw AccessDeniedError HttpClient return 403', async () => {
+    httpClient.request.mockResolvedValueOnce({ statusCode: 403 })
+
+    const promise = sut(PokemonParams)
+
+    await expect(promise).rejects.toThrow(new AccessDeniedError())
   })
 })
