86480f26111d4ea4199a2468c566b333a983abe8
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-17 10:04:09 -0300

    feat: ensure DeletePokemonUseCase throw AccessDeniedError HttpClient return 403

--- src/domain/use-cases/pokemon/delete-pokemon.ts
+++ src/domain/use-cases/pokemon/delete-pokemon.ts
@@ -1,9 +1,14 @@
 import { type HttpClient } from '@/domain/contracts/http'
+import { AccessDeniedError } from '@/domain/errors'
 
 type Setup = (url: string, httpClient: HttpClient<boolean>) => DeletePokemon
 type Input = { idPokemon: string }
 export type DeletePokemon = (input: Input) => Promise<void>
 
 export const DeletePokemonUseCase: Setup = (url, httpClient) => async ({ idPokemon }) => {
-  await httpClient.request({ url: `${url}/${idPokemon}`, method: 'delete' })
+  const { statusCode } = await httpClient.request({ url: `${url}/${idPokemon}`, method: 'delete' })
+  switch (statusCode) {
+    case 204: break
+    case 403: throw new AccessDeniedError()
+  }
 }
