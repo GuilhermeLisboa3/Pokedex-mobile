2473d3a9ec95698e0692f3af8ace1df54e536d5e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-17 14:03:09 -0300

    feat: ensure Favorite call GetDataPokemon with correct value

--- src/application/pages/favorite/index.tsx
+++ src/application/pages/favorite/index.tsx
@@ -1,19 +1,37 @@
 import { Container, Main, ListPokemon } from './styles'
-import { Footer } from '@/application/components'
+import { PokemonCardAnimation, Footer } from '@/application/components'
 import { type GetListFavoritePokemon } from '@/domain/use-cases/pokemon'
+import { type GetDataPokemon } from '@/domain/use-cases/api-pokemon'
+import { type ApiPokemon, type Pokemon } from '@/domain/models'
 
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { ScrollView } from 'react-native'
 import { useFocusEffect } from '@react-navigation/native'
 
 type Props = {
   getListFavoritePokemon: GetListFavoritePokemon
+  getDataPokemon: GetDataPokemon
 }
 
-export const Favorite: React.FC<Props> = ({ getListFavoritePokemon }) => {
+export const Favorite: React.FC<Props> = ({ getListFavoritePokemon, getDataPokemon }) => {
+  const [, setListFavoritePokemon] = useState<Pokemon[]>([])
+  const [listPokemon, setListPokemon] = useState<ApiPokemon[]>([])
+
   useFocusEffect(useCallback(() => {
-    getListFavoritePokemon()
+    getListFavoritePokemon().then(result => {
+      listPokemonHandler(result)
+    })
   }, []))
+
+  const listPokemonHandler = async (favoritePokemon: Pokemon[]): Promise<void> => {
+    const listPokemon = favoritePokemon.map(async (pokemon) => {
+      const dataPokemon = await getDataPokemon({ idOrName: pokemon.idPokemon })
+      return dataPokemon.pokemon
+    })
+    const pokemons = await Promise.all(listPokemon)
+    setListPokemon(pokemons)
+    setListFavoritePokemon(favoritePokemon)
+  }
 
   return (
     <>
@@ -24,6 +42,10 @@ export const Favorite: React.FC<Props> = ({ getListFavoritePokemon }) => {
       <Container>
         <Main>
           <ListPokemon>
+          { listPokemon.length > 0
+            ? listPokemon.map(pokemon => (<PokemonCardAnimation pokemon={pokemon} key={pokemon.id}/>))
+            : <p className='favorite-text'>Você não tem pokemons favoritado.</p>
+          }
           </ListPokemon>
         </Main>
         <Footer/>
