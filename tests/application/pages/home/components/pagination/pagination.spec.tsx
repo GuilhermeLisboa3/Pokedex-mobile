b08ff7fc9296c7e96dddb541a077da54e360ed90
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-18 19:01:43 -0300

    test: ensure Pagination call method with correct value

--- tests/application/pages/home/components/pagination/pagination.spec.tsx
+++ tests/application/pages/home/components/pagination/pagination.spec.tsx
@@ -2,9 +2,16 @@ import { Pagination } from '@/application/pages/home/components'
 
 import React from 'react'
 import { fireEvent, render, screen } from '@testing-library/react-native'
+import moti from 'moti'
 
 describe('Pagination', () => {
   const setPageSpy = jest.fn()
+  const useAnimationState = jest.spyOn(moti, 'useAnimationState')
+  const transitionToSpy = jest.fn()
+
+  beforeAll(() => {
+    useAnimationState.mockImplementation(jest.fn().mockImplementation(() => ({ transitionTo: transitionToSpy })))
+  })
   it('should call setPage with correct value', async () => {
     render(<Pagination count={50} perPage={25} page={2} setPage={setPageSpy}/>)
     fireEvent.press(screen.getAllByRole('link')[0])
@@ -27,5 +34,13 @@ describe('Pagination', () => {
     render(<Pagination count={50} perPage={25} page={1} setPage={setPageSpy}/>)
     fireEvent.press(screen.getAllByRole('link')[1])
     expect(setPageSpy).not.toHaveBeenCalled()
+  })
+
+  it('should call method with correct value', async () => {
+    render(<Pagination count={50} perPage={25} page={1} setPage={setPageSpy}/>)
+    fireEvent(screen.getAllByRole('link')[0], 'pressOut')
+    expect(transitionToSpy).toHaveBeenCalledWith('pressOut')
+    fireEvent(screen.getAllByRole('link')[1], 'pressOut')
+    expect(transitionToSpy).toHaveBeenCalledWith('pressOut')
   })
 })
